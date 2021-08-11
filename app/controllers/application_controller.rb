e665d14229e409e615a0a2d42c4e74d76412c03a
vinayak1923 <vinayagamoorthi27@gmail.com>
2021-08-11 16:38:57 +0530

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def home
+        render html: helpers.content_tag(:h1, 'Home Contorller')
+        #render html:"hello world"
+    end
 end
