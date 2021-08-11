6ba4076795323ace783a31b245181449518ad442
vinayak1923 <vinayagamoorthi27@gmail.com>
2021-08-11 19:20:16 +0530

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'users#index'
+  resources :users, except: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#home'
 end
