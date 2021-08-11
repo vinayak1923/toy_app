5e354920d2188eb5bc5ab5b7a03d183b3f07ae32
vinayak1923 <vinayagamoorthi27@gmail.com>
2021-08-11 17:56:34 +0530

    git commited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-    def home
-        render html: helpers.content_tag(:h1, 'Home Contorller')
-        #render html:"hello world"
-    end
+    
 end
